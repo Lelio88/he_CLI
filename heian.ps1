a2420bce3a3a06fca9f21aec19d5f34a934e2f84
Lelio Buton <buton1@live.fr>
2025-11-20 10:33:29 +0100

    Test réparation coutour

--- heian.ps1
+++ heian.ps1
@@ -2,9 +2,9 @@
 [Console]::InputEncoding = [System.Text.Encoding]::UTF8
 
 Write-Host ""
-Write-Host "============================================================================" -ForegroundColor DarkCyan
-Write-Host "============================================================================" -ForegroundColor Cyan
-Write-Host "======                                                                ======" -ForegroundColor Cyan
+Write-Host "======================================================================================" -ForegroundColor DarkCyan
+Write-Host "======================================================================================" -ForegroundColor Cyan
+Write-Host "======                                                                          ======" -ForegroundColor Cyan
 Write-Host "======              " -ForegroundColor Cyan -NoNewline
 Write-Host "██╗  ██╗███████╗██╗ █████╗ ███╗   ██╗" -ForegroundColor Magenta -NoNewline
 Write-Host "                  ======" -ForegroundColor Cyan
@@ -23,7 +23,7 @@ Write-Host "                  ======" -ForegroundColor Cyan
 Write-Host "======              " -ForegroundColor Cyan -NoNewline
 Write-Host "╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝" -ForegroundColor Magenta -NoNewline
 Write-Host "                  ======" -ForegroundColor Cyan
-Write-Host "======                                                                ======" -ForegroundColor Cyan
+Write-Host "======                                                                          ======" -ForegroundColor Cyan
 Write-Host "======  " -ForegroundColor Cyan -NoNewline
 Write-Host "███████╗███╗   ██╗████████╗███████╗██████╗ ██████╗ ██████╗ ██╗███████╗███████╗" -ForegroundColor DarkYellow -NoNewline
 Write-Host " ======" -ForegroundColor Cyan
